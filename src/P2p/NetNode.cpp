f71029678d183c103af13531fdd3df5d17b8cd6d
Pasquale Lembo <lembopasquale78@gmail.com>
2024-11-16 11:54:15 +0000

    Descrizione delle modifiche effettuate

--- src/P2p/NetNode.cpp
+++ src/P2p/NetNode.cpp
@@ -8,6 +8,7 @@
 
 #include <algorithm>
 #include <fstream>
+#include <boost/bind/bind.hpp>
 
 #include <boost/foreach.hpp>
 #include <boost/uuid/random_generator.hpp>
@@ -219,7 +220,11 @@ namespace CryptoNote
     s(m_config.m_peer_id, "peer_id");
   }
 
-#define INVOKE_HANDLER(CMD, Handler) case CMD::ID: { ret = invokeAdaptor<CMD>(cmd.buf, out, ctx,  boost::bind(Handler, this, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4)); break; }
+#define INVOKE_HANDLER(CMD, Handler) \
+    case CMD::ID: { \
+        ret = invokeAdaptor<CMD>(cmd.buf, out, ctx, boost::bind(Handler, this, _1, _2, _3, _4)); \
+        break; \
+    }
 
 
   int NodeServer::handleCommand(const LevinProtocol::Command& cmd, BinaryArray& out, P2pConnectionContext& ctx, bool& handled) {
