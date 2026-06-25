c634b6fdc983f5b4ef2be659bec6b5f0c4040ad6
Your Name <you@example.com>
2026-06-25 17:01:48 +0000

    FULL MIRROR SERVER → MEVACOIN

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -4111,7 +4111,7 @@ void wallet2::refresh(bool trusted_daemon, uint64_t start_height, uint64_t & blo
   // pull the first set of blocks
   get_short_chain_history(short_chain_history, (m_first_refresh_done || trusted_daemon) ? 1 : FIRST_REFRESH_GRANULARITY);
   m_run.store(true, std::memory_order_relaxed);
-  if (start_height > m_blockchain.size() || m_refresh_from_block_height > m_blockchain.size() || m_skip_to_height > m_blockchain.size()) {
+  if (m_blockchain.size() == 0 || start_height > m_blockchain.size() || m_refresh_from_block_height > m_blockchain.size() || m_skip_to_height > m_blockchain.size()) {
     if (!start_height)
       start_height = std::max(m_refresh_from_block_height, m_skip_to_height);;
     // we can shortcut by only pulling hashes up to the start_height
